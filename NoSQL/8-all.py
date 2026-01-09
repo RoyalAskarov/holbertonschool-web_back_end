a63780cf6f95a6fa13714ad8c42a8d785566beff
Royal Askarov <royalaskar42@gmail.com>
2026-01-09 16:45:45 +0400

    cm8

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+""" Module that provides a function to list all documents in a collection """
+
+
 def list_all(mongo_collection):
     """ Lists all documents in a collection """
     return list(mongo_collection.find())
